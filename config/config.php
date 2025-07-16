8c10b62445b1936c1104ef0bed243eb1e3c492ef
Vitor Efraim <vitor.efraim@onfly.com.br>
2025-07-16 12:01:32 -0300

    chore: add app_secret to config, change logger.php and add new routes

--- config/config.php
+++ config/config.php
@@ -17,12 +17,12 @@ use function Hyperf\Support\env;
 return [
     'app_name' => env('APP_NAME', 'skeleton'),
     'app_env' => env('APP_ENV', 'dev'),
+    'app_secret' => env('APP_SECRET', 'skeleton'),
     'scan_cacheable' => env('SCAN_CACHEABLE', false),
     StdoutLoggerInterface::class => [
         'log_level' => [
             LogLevel::ALERT,
             LogLevel::CRITICAL,
-            LogLevel::DEBUG,
             LogLevel::EMERGENCY,
             LogLevel::ERROR,
             LogLevel::INFO,
--- config/routes.php
+++ config/routes.php
@@ -9,10 +9,18 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use App\Controller\AuthController;
+use App\Middleware\GlobalMiddleware;
 use Hyperf\HttpServer\Router\Router;
+use Hyperf\Validation\Middleware\ValidationMiddleware;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
-
-Router::get('/favicon.ico', function () {
-    return '';
-});
+Router::addGroup('/auth', function () {
+    Router::post('/login', [AuthController::class, 'signIn']);
+    Router::post('/register', [AuthController::class, 'signUp']);
+}, [
+    'middleware' => [
+        GlobalMiddleware::class,
+        ValidationMiddleware::class
+    ]
+]);
